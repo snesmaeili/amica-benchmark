950bfa0f3fca19a933ea7697d05939ab7a079a43
Sina Esmaeili <sina.esmaeili@umontreal.ca>
2026-05-21 00:23:21 -0400

    Revert "cc_benchmark: import fir cluster scaffolding from amica-python"
    This reverts commit 1baf53a9bdc4a0d66ab0214147d2370285836b74.

--- scripts/cc_benchmark/plot_v3_comparison.py
+++
@@ -1,7 +0,0 @@
-"""Thin shim: re-exports from ``amica_python.benchmark.viz.headline``."""
-from amica_python.benchmark.viz.headline import *  # noqa: F401,F403
-from amica_python.benchmark.viz.headline import main  # noqa: F401
-
-
-if __name__ == "__main__":
-    main()
